05b0e43473d36746868c9ec4732b6be6ad977c84
brian-big <briannbig@gmail.com>
2022-07-17 08:02:58 +0300

    add gateway service

--- api-gateway/src/main/java/com/briannbig/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/briannbig/gateway/ApiGatewayApplication.java
@@ -1,13 +1,17 @@
-package com.briannbig.customers;
+package com.briannbig.gateway;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
-public class CustomerApplication {
+@SpringBootApplication
+public class ApiGatewayApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(CustomerApplication.class, args);
+        SpringApplication.run(
+                ApiGatewayApplication.class, args
+        );
     }
 }
